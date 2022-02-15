895e6987b20556be9fc9088d890d227946feb3f8
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2022-02-15 17:38:46 +0300

    Delete instances file

--- instances.js
+++
@@ -1,8 +0,0 @@
-const hostnames = {
-	instagram_hostname: "bibliogram.pussthecat.org";
-	twitter_hostname: "nitter.42l.fr",
-	reddit_hostname: "libreddit.spike.codes",
-	youtube_hostname: "vid.puffyan.us"
-};
-
-export { hostnames };
