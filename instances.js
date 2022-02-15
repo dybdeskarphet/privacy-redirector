d592d19164f04c8e4ac64006eb2195dadc19f065
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2022-02-15 17:35:57 +0300

    Add instances file

---
+++ instances.js
@@ -0,0 +1,8 @@
+const hostnames = {
+	instagram_hostname: "bibliogram.pussthecat.org";
+	twitter_hostname: "nitter.42l.fr",
+	reddit_hostname: "libreddit.spike.codes",
+	youtube_hostname: "vid.puffyan.us"
+};
+
+export { hostnames };
