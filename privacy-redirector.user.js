8c07466686f53c9e99e46894c22a8e01a55acc6f
Ahmet Arda Kavakci <ahmetardakavakci@gmail.com>
2023-07-29 21:11:07 +0300

    Change file name

--- privacy-redirector.user.js
+++ privacy-redirector.user.js
@@ -61,9 +61,9 @@
 // @author Ahmet Arda Kavakcı
 // @license GPLv3
 // @version 1.4.5
-// @downloadURL https://raw.githubusercontent.com/dybdeskarphet/privacy-redirector/main/privacy-redirector.js
+// @downloadURL https://raw.githubusercontent.com/dybdeskarphet/privacy-redirector/main/privacy-redirector.user.js
 // @supportURL https://github.com/dybdeskarphet/privacy-redirector
-// @updateURL https://raw.githubusercontent.com/dybdeskarphet/privacy-redirector/main/privacy-redirector.js
+// @updateURL https://raw.githubusercontent.com/dybdeskarphet/privacy-redirector/main/privacy-redirector.user.js
 // @run-at document-start
 // @match *://*.bandcamp.com/*
 // @match *://*.fandom.com/*
